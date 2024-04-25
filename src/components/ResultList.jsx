6fe5c12f44384af18b39523dc5f8f88166f9e433
tranba <janetran251@gmail.com>
2024-04-25 21:44:42 +1000

    finish with save playlist function

--- src/components/ResultList.jsx
+++ src/components/ResultList.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './ResultList.css'
+import DeleteIcon from '@mui/icons-material/Delete';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 export const ResultList = ({resultList}) => {
-
+const [chosen, setChosen]=useState([]);
+const handleClick =(item)=>{
+    if (chosen.includes(item.title)) {
+        alert(`${item.title} is already chosen.`);
+        return; // Return early if the item is already chosen
+      }
+      // Add the item's title to the chosen array
+      setChosen((prev) => [...prev, item.title]);
+};
+const handleClear =(id)=>{
+    let reduceList=[...chosen];
+    reduceList.splice(id,1);
+    setChosen(reduceList)
+};
   return (
+
+    <div className='result-wrapper'>
+
+
+
     <div className='display'>
         <h1>Results</h1>
         <div className='display-list'>
@@ -12,12 +31,34 @@ export const ResultList = ({resultList}) => {
             <div key={index} className='result-name'>
                 {item.title}
             </div>
-            <div className='addIcon'>
+            <div className='icon' onClick={()=>handleClick(item)}>
                 <AddCircleOutlineIcon />
             </div>
             </div>
         ))} 
         </div>
+    </div>
+
+
+
+    <div className='save-result'>
+        <h1>Playlist</h1>
+
+        <div className='display-list'>
+        {chosen.map((item,id)=>(
+            <div className='eachResult'>
+            <div key={id} className='result-name'>{item}</div>
+            <div className='icon' onClick={()=>handleClear(id)}>
+            <DeleteIcon />
+            </div>
+        </div>
+        ))}
+        </div>
+    
+    </div>
+
+
+
     </div>
   )
 }
--- src/components/SaveResult.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SaveResult = () => {
-  return (
-    <div>SaveResult</div>
-  )
-}
